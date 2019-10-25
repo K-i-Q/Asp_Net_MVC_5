55ffd8e4fe6e1490135766379eae439098d7554c
Carlos <carlos.oli1505@gmail.com>
2019-10-25 00:48:40 -0300

    Instalação do EntityFramework; Adição da Classe de Contexto e string de conexão
    Instalação do EntityFramework; Adição da Classe de Contexto e string de conexão

---
+++ Asp_Net_MVC_5/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Net_MVC_5.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("Asp_Net_MVC_CS") { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
---
+++ Asp_Net_MVC_5/Models/Fabricante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Net_MVC_5.Models
+{
+    public class Fabricante
+    {
+        public long FabricanteId { get; set; }
+        public string Nome { get; set; }
+    }
+}
