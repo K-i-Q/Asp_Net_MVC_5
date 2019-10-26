5af27df7509265e93744a0f82e73d54012079795
Carlos <carlos.oli1505@gmail.com>
2019-10-26 12:45:28 -0300

    Criado novo Controlador para o modelo de Fabricante e implementado View para Action Index
    Criado novo Controlador para o modelo de Fabricante e implementado View para Action Index

---
+++ Asp_Net_MVC_5/Controllers/FabricantesController.cs
@@ -0,0 +1,19 @@
+﻿using Asp_Net_MVC_5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp_Net_MVC_5.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private Context context = new Context();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+    }
+}
